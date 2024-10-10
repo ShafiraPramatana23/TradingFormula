f65913d64967755b498fdef32b2f2ddbc10245f8
ShafiraPramatana <firapramatana03@gmail.com>
2024-10-10 15:10:52 +0700

    first commit

---
+++ Sources/TradingFormula/TradingFormula.swift
@@ -0,0 +1,14 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public struct TradingFormula {
+    public init() {}
+    
+    public func helloWorld() {
+        print("Hello, World!")
+    }
+    
+    public func getTextHihi() -> String {
+        return "Shafiraaaa"
+    }
+}
